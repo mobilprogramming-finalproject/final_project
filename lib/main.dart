30b6f2ad81f5afeb1e01512faa6f7c5157fb98ea
Gizem <gizem.demrz@gmail.com>
2021-01-08 15:22:53 +0300

    yorum girildi

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 //dhahahahah
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  //gjhbghj
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

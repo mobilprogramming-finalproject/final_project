3105c99d4cd7047f1989580de14793bf58db9243
Gizem <gizem.demrz@gmail.com>
2021-01-17 15:40:32 +0300

    Fix

---
+++ lib/scr/providers/product.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import '../helpers/product.dart';
+import '../models/products.dart';
+
+class ProductProvider with ChangeNotifier{
+  ProductServices _productServices = ProductServices();
+  List<ProductModel> products = [];
+  List<ProductModel> productsByCategory = [];
+  List<ProductModel> productsByRestaurant = [];
+  List<ProductModel> productsSearched = [];
+
+
+
+  ProductProvider.initialize(){
+    loadProducts();
+  }
+
+  loadProducts()async{
+    products = await _productServices.getProducts();
+    notifyListeners();
+  }
+
+  Future loadProductsByCategory({String categoryName})async{
+    productsByCategory = await _productServices.getProductsOfCategory(category: categoryName);
+    notifyListeners();
+  }
+
+  Future loadProductsByRestaurant({String restaurantId})async{
+    productsByRestaurant = await _productServices.getProductsByRestaurant(id: restaurantId);
+    notifyListeners();
+  }
+
+
+
+  Future search({String productName})async{
+    productsSearched = await _productServices.searchProducts(productName: productName);
+    print("THE NUMBER OF PRODUCTS DETECTED IS: ${productsSearched.length}");
+    print("THE NUMBER OF PRODUCTS DETECTED IS: ${productsSearched.length}");
+    print("THE NUMBER OF PRODUCTS DETECTED IS: ${productsSearched.length}");
+
+    notifyListeners();
+  }
+
+
+}

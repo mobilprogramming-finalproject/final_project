2c57370845b0e71d7817b710e16867daebf210b7
Gizem <gizem.demrz@gmail.com>
2021-01-17 14:20:18 +0300

    Versiyonlar güncellendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,49 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:final_project/scr/providers/app.dart';
+import 'package:final_project/scr/providers/category.dart';
+import 'package:final_project/scr/providers/restaurant.dart';
+import 'package:final_project/scr/providers/product.dart';
+import 'package:final_project/scr/providers/user.dart';
+import 'package:final_project/scr/screens/home.dart';
+import 'package:final_project/scr/screens/login.dart';
+import 'package:final_project/scr/screens/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:final_project/scr/widgets/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
-import 'Setup/LoginPage.dart';
-
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: AppProvider()),
+        ChangeNotifierProvider.value(value: UserProvider.initialize()),
+        ChangeNotifierProvider.value(value: CategoryProvider.initialize()),
+        ChangeNotifierProvider.value(value: RestaurantProvider.initialize()),
+        ChangeNotifierProvider.value(value: ProductProvider.initialize()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Food App',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+          ),
+          home: ScreensController())));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ScreensController extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        //nolur
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginPage(),
-    );
+    final auth = Provider.of<UserProvider>(context);
+    switch (auth.status) {
+      case Status.Uninitialized:
+        return Splash();
+      case Status.Unauthenticated:
+      case Status.Authenticating:
+        return LoginScreen();
+      case Status.Authenticated:
+        return Home();
+      default:
+        return LoginScreen();
+    }
   }
 }
